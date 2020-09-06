23249cbd70ae03db5a4816e938dd8b671a01f386
Thomas Shih <tt7919@gmail.com>
2020-09-06 15:23:04 +0800

    style: set class on dayViewer

--- src/components/day-viewer.jsx
+++ src/components/day-viewer.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import '../style/calendar.scss';
 
 const propTypes = {
 };
 
-function DayViewer(dayOfWeek, date, availableStartTime, availableEntTime) {
+function DayViewer({ dayOfWeek, date, availableStartTime, availableEntTime }) {
   // generateOneDataAvailableTime(availableStartTime, availableEntTime) {
   // }
 
   return (
-    <div className="day">
-      <div className="dayOfWeek">{dayOfWeek}</div>
-      <div className="date">{date}</div>
-      <ul className="time">
+    <div className="dayContainer">
+      <div className="dayOfWeek textCenter fontSize-l">{dayOfWeek}</div>
+      <div className="date textCenter fontSize-l">{date}</div>
+      <ul className="time fontSize-s">
         <li>00:00</li>
         <li>00:30</li>
         <li>01:00</li>
