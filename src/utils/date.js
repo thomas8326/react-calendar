9ace040a5aa024d6a4151768fc5c1e6ce3294d15
Thomas Shih <tt7919@gmail.com>
2020-09-09 00:03:32 +0800

    feat: put get current week to date class.

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,9 +1,21 @@
 export class MyDate extends Date {
-  current;
+  current = new Date();
+  dayOfWeekMap = new Map([
+    [0, 'Sun'],
+    [1, 'Mon'],
+    [2, 'Tue'],
+    [3, 'Wed'],
+    [4, 'Thu'],
+    [5, 'Fri'],
+    [6, 'Sat'],
+  ]);
 
   constructor(myDate) {
     super();
-    this.current = myDate;
+    if (myDate) {
+      this.current = new Date(myDate);
+      console.log('In');
+    }
   }
 
   getYear() {
@@ -28,6 +40,28 @@ export class MyDate extends Date {
 
   getMinutes() {
     return this.current.getMinutes();
+  }
+
+  getCurrentWeek() {
+    const firstDayOfWeek = this.current.getDate() - this.current.getDay();
+    const lastDayOfWeek = firstDayOfWeek + 6;
+
+    const week = new Array(lastDayOfWeek - firstDayOfWeek + 1).fill().map((_, index) => {
+      const myDate = new MyDate(this.current.setDate(firstDayOfWeek + index));
+      return {
+        key: myDate.getKey(),
+        year: myDate.getYear(),
+        month: myDate.getMonth(),
+        date: myDate.getDate(),
+        dayOfWeek: this.dayOfWeekMap.get(index),
+      }
+    });
+
+    return {
+      weekStartDate: new MyDate(this.current.setDate(firstDayOfWeek)).getTime(),
+      weekEndDate: new MyDate(this.current.setDate(lastDayOfWeek)).getTime(),
+      week: week
+    };
   }
 
 }
