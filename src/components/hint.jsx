4a38b0b6aa8da752c790bfd73f36422e8f1f347d
Thomas Shih <tt7919@gmail.com>
2020-09-11 12:29:32 +0800

    style: header rwd

--- src/components/hint.jsx
+++ src/components/hint.jsx
@@ -6,9 +6,9 @@ const propTypes = {
 function Hint() {
   return (
     <div className="hint fontSize-s">
-      <span>
+      <div className="hintText">
         * All the timings listed are in your timezone:
-      </span>
+      </div>
       <div className="timezone">
         Taipei (GMT+08:00)
       </div>
